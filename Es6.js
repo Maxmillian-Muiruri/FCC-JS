9b66af19d776930e8cff3897fddcda19a6fa0505
Maxmillian-Muiruri <maxmillianmuiruri@gmail.com>
2023-04-03 19:49:26 +0300

    fcc practise

--- Es6.js
+++ Es6.js
@@ -1,20 +1,45 @@
 // Compare Scopes of the var and let keywords
 // Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
 function checkScope() {
-  let i = 'function scope';
+  let i = "function scope";
   if (true) {
-   let i = 'block scope';
-    console.log('Block scope i is: ', i);
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
   }
-  console.log('Function scope i is: ', i);
+  console.log("Function scope i is: ", i);
   return i;
 }
 // An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments
 const s = [5, 7, 2];
 function editInPlace() {
-
   s[0] = 2;
   s[1] = 5;
   s[2] = 7;
 }
 editInPlace();
+
+// Prevent Object Mutation
+function freezeObj() {
+  try {
+    const MATH_CONSTANTS = {
+      PI: 3.14,
+    };
+    console.log(MATH_CONSTANTS.PI);
+    MATH_CONSTANTS.PI = 99;
+
+    console.log(MATH_CONSTANTS.PI);
+    return MATH_CONSTANTS.PI;
+  } catch (ex) {
+    console.log(ex);
+    return;
+  }
+}
+const PI = freezeObj();
+
+// Use Arrow Functions to Write Concise Anonymous Functions
+const magic = () => new Date();
+console.log(magic());
+// Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+const increment = (number, value = 1) => number + value;
+console.log(increment(5, 2));
+console.log(increment(5));
--- for loop.js
+++ for loop.js
@@ -10,7 +10,6 @@ console.log(myArray);
 const myArra = [];
 for (var i = 9; i > 0; i -= 2) {
   myArra.push(i);
-
 }
 
 console.log(myArra);
@@ -19,48 +18,50 @@ console.log(myArra);
 // Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
 //solution
 const myArr = [2, 3, 4, 5, 6];
-let total = 0
+let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
-console.log(total)
+console.log(total);
 // Nesting For Loops
 // Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
 function multiplyAll(arr) {
   let product = 1;
-for (let i = 0; i < arr.length; i++) { 
-  console.log(arr);
-for ( let j = 0;  j < arr[i].length; j++) { 
-  product *= arr[i][j];
-  console.log(arr[i][j]);
-}
-}
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr);
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+      console.log(arr[i][j]);
+    }
+  }
   return product;
 }
 
-
-console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+console.log(
+  multiplyAll([
+    [1, 2],
+    [3, 4],
+    [5, 6, 7],
+  ])
+);
 
 // Iterate with JavaScript Do...While Loops
 
 const Array = [];
 
-
 do {
   Array.push(i);
   i++;
-} while (i < 5) 
-console.log(Array)
-console.log(i)
+} while (i < 5);
+console.log(Array);
+console.log(i);
 
 // Replace Loops using Recursion
 
 function sum() {
-  
-if (n <= 0) {
-  return 0;
-} else {
-  return sum(arr, n - 1) + arr[n - 1];
-}
-
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
 }
